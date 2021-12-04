a2078588af5f3c93bde5e42657323d3db4c52789
Fábio Albini Braga <fabio.albini@diffagency.com>
2021-12-04 12:39:56 -0300

    gc

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,7 +5,7 @@
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-server ENV.fetch(:SERVER_PUBLIC_IP), user: "ec2-user", roles: %w{app}
+server ENV.fetch('SERVER_PUBLIC_IP'), user: "ec2-user", roles: %w{app}
 
 set :branch, "main"
 
