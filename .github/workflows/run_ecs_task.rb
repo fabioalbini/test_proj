b519314606f4fddd4ba7406e81bc461def62a0c3
Fábio Albini Braga <fabio.albini@diffagency.com>
2021-12-05 12:25:50 -0300

    dep

--- .github/workflows/run_ecs_task.rb
+++ .github/workflows/run_ecs_task.rb
@@ -6,14 +6,22 @@ SUBNETS=["subnet-2210f56b"]
 
 client = Aws::ECS::Client.new
 
-resp = client.run_task({
+run_task_response = client.run_task({
   launch_type: "FARGATE",
   cluster: CLUSTER,
-  task_definition: ENV.fetch("TASK_DEFINITION"),
+  task_definition: ENV.fetch("ECS_TASK_DEFINITION"),
   network_configuration: {
     awsvpc_configuration: {
-      subnets: SUBNETS,
+      subnets: [ENV.fetch("ECS_SUBNET")],
       assign_public_ip: "ENABLED"
     }
   }
+
 })
+
+wait_while = ["PROVISIONING", "PENDING", "ACTIVATING", "RUNNING"]
+loop do
+  task = client.describe_tasks(cluster: "deploy-cluster", tasks: [run_task_response.tasks.first.task_arn]).first
+  sleep 2
+  return unless wait_while.include?(task.last_status)
+end
