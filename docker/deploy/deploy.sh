99c3c7ccd50c5a0477c1dc083f7ddb3b6715fb54
Fábio Albini Braga <fabio.albini@diffagency.com>
2021-12-07 07:04:16 -0300

    test

--- docker/deploy/deploy.sh
+++ docker/deploy/deploy.sh
@@ -1,7 +1,7 @@
-echo "$EC2_SSH_KEY2"
-echo "$EC2_SSH_KEY2" > ~/.ec2/test.pem
-echo "RESPONSE"
-cat ~/.ec2/test.pem
-echo "$EC2_SSH_KEY" > ~/.ec2/deploy.pem
+# echo "$EC2_SSH_KEY2"
+echo "$EC2_SSH_KEY2" > ~/.ec2/deploy.pem
+# echo "RESPONSE"
+# cat ~/.ec2/test.pem
+# echo "$EC2_SSH_KEY" > ~/.ec2/deploy.pem
 chmod 400 ~/.ec2/deploy.pem
 bundle exec cap staging deploy
